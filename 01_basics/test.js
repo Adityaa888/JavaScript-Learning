d1a98f6d7d329b9187c3f7bf3556f909d87a3299
Adityaa888 <145027209+Adityaa888@users.noreply.github.com>
2024-05-10 14:53:53 +0000

    datatypes done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,21 @@
-console.log("You are a best Warrior ");
+console.log("You are a Warrior ");
+
+let value = 3;
+let negval = -value ;
+//  console.log(negval);
+//  console.log(++value);
+//  console.log(value);
+//  console.log(1>true);
+//  console.log(true === true);
+
+//  # primitive Datatypes 
+//  7 types : String , NUmber , Boolean , null , undefined ,Symbol ,BigInt 
+
+// Reference (Non-premitive datatype)
+// Array , Objets , Functions ;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
